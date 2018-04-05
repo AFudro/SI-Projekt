0c952fe33826f8617a8f6f4eb7974b26c7088b72
AFudro <headcrab96@gmail.com>
2018-04-05 11:47:34 +0200

    grid

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
 import pygame, sys
 from pygame.locals import *
 
-
-class Grid:
-    def __init__(self, x,y ):
-        pass
-
-
-
 class Cell(pygame.Rect):
-    def __init__(self, x, y):
+    def __init__(self, x, y, s):
         self.color= (0, 0, 255)
-        self.s=20
+        self.s=s
         super(Cell, self).__init__(x, y, self.s, self.s)
     def draw(self,screen):
-        pygame.draw.rect(screen, color , self)
+        pygame.draw.rect(screen, self.color , self,1)
+
+class Grid:
+    def __init__(self):
+        self.size=50
+        self.cellSize=20
+        self.cells=[[0 for x in range(self.size)] for y in range(self.size)]
+
+        for i in range(self.size):
+            for j in range(self.size):
+                self.cells[i][j]= Cell(i*self.cellSize,j*self.cellSize,self.cellSize)
+
+    def draw(self,screen):
+        for i in range(self.size):
+            for j in range(self.size):
+                self.cells[i][j].draw(screen)
+
+
+
+
 
 
 class Agent:
@@ -24,14 +36,10 @@ class Agent:
 pygame.init()
 screen = pygame.display.set_mode((400, 300))
 
-
-
-cell= Cell(11,26)
-cell.draw(screen)
-
-
-
-
+# cell= Cell(11,26)
+# cell.draw(screen)
+grid= Grid()
+grid.draw(screen)
 
 while True:
     for event in pygame.event.get():
