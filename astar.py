b85a693efd47697f8f6f4ce5d5143cea134a5721
LukaszLejman <LukaszLejman96@wp.pl>
2018-05-22 23:09:30 +0200

    a* fix, visual upgrade, cost function

--- astar.py
+++ astar.py
@@ -2,75 +2,84 @@ from math import *
 import operator
 from queue import *
 
-index=0
+index = 0
+
 
 class Node:
-    def __init__(self,x,y,direction,endx,endy,action,gScore):
-        self.gScore=gScore
-        self.hScore=sqrt(((endx-x)**2)+((endy-y)**2))
-        self.fScore=self.gScore+self.hScore
-        self.visitedBy=0
-        self.x=x
-        self.y=y
-        self.direction=direction
-        self.action=action
+    def __init__(self, x, y, direction, endx, endy, action, gScore):
+        self.gScore = gScore
+        self.hScore = sqrt(((endx - x)**2) + ((endy - y)**2))
+        self.fScore = self.gScore + self.hScore
+        self.x = x
+        self.y = y
+        self.direction = direction
+        self.action = action
 
     def __lt__(self, other):
-        return self.fScore <other.fScore
+        return self.fScore < other.fScore
 
     def update(self):
-        self.fScore=self.gScore+self.hScore
+        self.fScore = self.gScore + self.hScore
 
-def generateNeighbors(current,endx,endy,map):
-    neighbors=[]
-    #print(current.x, current.y-1)
+
+def generateNeighbors(current, endx, endy, map):
+    neighbors = []
+    # print(current.x, current.y-1)
     if (current.direction[0] == "N"):
-         if((current.y-1)>=0):
-             if(map[int(current.y-1)][int(current.x)]!='0'):
-                neighbors.append(Node(current.x, current.y-1, current.direction, endx, endy,"move",current.gScore + 1))  # move
+         if((current.y - 1) >= 0):
+             if(map[int(current.y - 1)][int(current.x)] != '0'):
+                neighbors.append(Node(current.x, current.y - 1, current.direction, endx, endy, "move",
+                                 current.gScore + current.gScore + int(map[int(current.y - 1)][int(current.x)])))  # move
     if (current.direction[0] == "S"):
-         if((current.y+1)<len(map)):
-             if(map[int(current.y+1)][int(current.x)]!='0'):
-                neighbors.append(Node(current.x, current.y+1, current.direction, endx, endy,"move",current.gScore + 1))  # move
+         if((current.y + 1) < len(map)):
+             if(map[int(current.y + 1)][int(current.x)] != '0'):
+                neighbors.append(Node(current.x, current.y + 1, current.direction, endx, endy, "move",
+                                 current.gScore + current.gScore + int(map[int(current.y + 1)][int(current.x)])))  # move
     if (current.direction[0] == "W"):
-         if((current.x-1)>=0):
-             if(map[int(current.y)][int(current.x-1)]!='0'):
-                neighbors.append(Node(current.x-1, current.y, current.direction, endx, endy,"move",current.gScore + 1))  # move
+         if((current.x - 1) >= 0):
+             if(map[int(current.y)][int(current.x - 1)] != '0'):
+                neighbors.append(Node(current.x - 1, current.y, current.direction, endx, endy, "move",
+                                 current.gScore + current.gScore + int(map[int(current.y)][int(current.x - 1)])))  # move
     if (current.direction[0] == "E"):
-         if((current.x+1)<len(map[0])):
-            if(map[int(current.y)][int(current.x+1)]!='0'):
-                neighbors.append(Node(current.x+1, current.y, current.direction, endx, endy,"move",current.gScore + 1))  # move
+         if((current.x + 1) < len(map[0])):
+            if(map[int(current.y)][int(current.x + 1)] != '0'):
+                neighbors.append(Node(current.x + 1, current.y, current.direction, endx, endy, "move",
+                                 current.gScore + current.gScore + int(map[int(current.y)][int(current.x + 1)])))  # move
 
     neighbors.append(
-        Node(current.x, current.y, [current.direction[3]] + current.direction[:3], endx, endy,"rotateLeft",current.gScore + 1))  # rotate left
+        Node(current.x, current.y, [current.direction[3]] + current.direction[:3], endx, endy, "rotateLeft", current.gScore + 1))  # rotate left
     neighbors.append(
-        Node(current.x, current.y, current.direction[1:] + [current.direction[0]], endx, endy,"rotateRight",current.gScore + 1))  # rotate right
+        Node(current.x, current.y, current.direction[1:] + [current.direction[0]], endx, endy, "rotateRight", current.gScore + 1))  # rotate right
 
     return neighbors
 
-def search(startx,starty,endx,endy,startdirection,map):
+
+def search(startx, starty, endx, endy, startdirection, map):
     start = Node(startx, starty, startdirection, endx, endy, 'start', 0)
 
     closedSet = set()
     openSet = PriorityQueue()
-    openSet.put((start.fScore,start.x,start.y,start.direction[0],start))
-    start.gScore=0
+    openSet.put((start.fScore, start.x, start.y, start.direction[0], start))
+    start.gScore = 0
     cameFrom = {}
 
     while not openSet.empty():
         current = openSet.get()[4]
-        if current.hScore==0:
+        if current.hScore == 0:
             return reconstruct_path(cameFrom, current)
-        closedSet.add(str(current.x)+','+str(current.y)+','+current.direction[0]+',')
+        closedSet.add(str(current.x) + ',' + str(current.y) +
+                      ',' + current.direction[0] + ',')
 
-        for neighbor in generateNeighbors(current,endx,endy,map):
-            if str(neighbor.x)+','+str(neighbor.y)+','+neighbor.direction[0]+',' in closedSet:
+        for neighbor in generateNeighbors(current, endx, endy, map):
+            if str(neighbor.x) + ',' + str(neighbor.y) + ',' + neighbor.direction[0] + ',' in closedSet:
                 continue
-            if (neighbor.fScore,neighbor.x,neighbor.y,neighbor.direction[0],neighbor) not in openSet.queue:
-                openSet.put((neighbor.fScore,neighbor.x,neighbor.y,neighbor.direction[0],neighbor))
-            tentative_gScore = current.gScore + 1
-            if tentative_gScore > neighbor.gScore:
-                continue
+            if (neighbor.fScore, neighbor.x, neighbor.y, neighbor.direction[0], neighbor) not in openSet.queue:
+                openSet.put((neighbor.fScore, neighbor.x,
+                            neighbor.y, neighbor.direction[0], neighbor))
+            if neighbor.action == "move":
+                tentative_gScore = current.gScore + int(map[int(neighbor.y)][int(neighbor.x)])
+            else:
+                tentative_gScore = current.gScore + int(map[int(neighbor.y)][int(neighbor.x)])
             cameFrom[neighbor]=current
             neighbor.gScore=tentative_gScore
             neighbor.update()
@@ -78,8 +87,8 @@ def search(startx,starty,endx,endy,startdirection,map):
     return 0
 
 def reconstruct_path(cameFrom, current):
-    total_path = [current.action]
-    while current.action!="start":
+    total_path=[current.action]
+    while current.action != "start":
         current=cameFrom[current]
         total_path.append(current.action)
     total_path.reverse()
--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import pygame, sys
+import pygame
+import sys
 import astar
 
 
 def loadMap():
-    array=[]
+    array = []
     with open('map.txt') as f:
         content = f.read().splitlines()
         for line in content:
@@ -12,41 +13,56 @@ def loadMap():
     return array
 
 
-
 class Cell(pygame.Rect):
     def __init__(self, x, y, s, wall):
-        self.color= (0, 0, 0)
-        self.s=s
-        self.wall=wall
+        self.color = (0, 0, 0)
+        self.s = s
+        self.wall = wall
+        self.image = pygame.image.load("agent.png")
+        self.image = pygame.transform.scale(self.image, (40, 40))
         super(Cell, self).__init__(x, y, self.s, self.s)
-    def draw(self,screen,color,fill):
-        pygame.draw.rect(screen, color, self,fill)
+
+    def draw(self, screen, color, fill):
+        pygame.draw.rect(screen, color, self, fill)
+        screen.blit(self.image, self)
+
 
 class Grid:
     def __init__(self, map, cellSize):
-        self.cellSize=cellSize
-        self.ysize=len(map)
-        self.xsize=len(map[0])
-        self.cells=[[0 for x in range(self.xsize)] for y in range(self.ysize)]
+        self.cellSize = cellSize
+        self.ysize = len(map)
+        self.xsize = len(map[0])
+        self.cells = [[0 for x in range(self.xsize)]
+                      for y in range(self.ysize)]
         for i in range(self.ysize):
             for j in range(self.xsize):
-                if(map[i][j]=='1'):
-                    self.cells[i][j]= Cell(j*self.cellSize,i*self.cellSize,self.cellSize,1)
-                elif (map[i][j]=='2'):
-                    self.cells[i][j]= Cell(j*self.cellSize,i*self.cellSize,self.cellSize,2)
-                else:
-                    self.cells[i][j]= Cell(j*self.cellSize,i*self.cellSize,self.cellSize,0)
+                self.cells[i][j] = Cell(
+                    j * self.cellSize, i * self.cellSize, self.cellSize, int(map[i][j]))
 
-    def draw(self,screen):
+    def draw(self, screen):
         for i in range(self.ysize):
             for j in range(self.xsize):
-                if(self.cells[i][j].wall==0):
-                    self.cells[i][j].draw(screen,(255,0,0),0)
-                if(self.cells[i][j].wall==1):
-                    self.cells[i][j].draw(screen,(255,255,255),0)
-
-                if(self.cells[i][j].wall==2):
-                    self.cells[i][j].draw(screen,(120,100,0),0)
+                if(self.cells[i][j].wall == 0):
+                    self.cells[i][j].draw(screen, (255, 0, 0), 0)
+                    if( ((i*j)+j+i) % 6 ==0):
+                        self.cells[i][j].image = pygame.transform.scale(pygame.image.load("tree.jpg"), (40, 40))
+                    else:
+                        self.cells[i][j].image = pygame.transform.scale(pygame.image.load("house.jpg"), (40, 40))
+                if(self.cells[i][j].wall == 1):
+                    self.cells[i][j].image = pygame.transform.scale(pygame.image.load("road.jpg"), (40, 40))
+                    self.cells[i][j].draw(screen, (112, 112, 112), 0)
+                if(self.cells[i][j].wall == 2):
+                    self.cells[i][j].image = pygame.transform.scale(pygame.image.load("road2.jpg"), (40, 40))
+                    self.cells[i][j].draw(screen, (95, 71, 71), 0)
+                if(self.cells[i][j].wall == 3):
+                    self.cells[i][j].image = pygame.transform.scale(pygame.image.load("road3.jpg"), (40, 40))
+                    self.cells[i][j].draw(screen, (95, 71, 71), 0)
+                if(self.cells[i][j].wall == 9):
+                    self.cells[i][j].image = pygame.transform.scale(pygame.image.load("oil.jpg"), (40, 40))
+                    self.cells[i][j].draw(screen, (0, 0, 255), 0)
+                if(self.cells[i][j].wall == 8):
+                    self.cells[i][j].image = pygame.transform.scale(pygame.image.load("puddle.jpg"), (40, 40))
+                    self.cells[i][j].draw(screen, (0, 255, 0), 0)
 
     def getCellSize(self):
         return self.cellSize
@@ -55,51 +71,66 @@ class Grid:
 class Agent(pygame.sprite.Sprite):
     def __init__(self, size):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.image.load("agent.png")
+        self.image = pygame.image.load("agent2.png")
         self.image = pygame.transform.scale(self.image, (size, size))
         self.rect = self.image.get_rect()
         self.rect.center = (100, 100)
-        self.directions=["W","N","E","S"]
-        self.size=size
-        self.path=[]
+        self.directions = ["W", "N", "E", "S"]
+        self.size = size
+        self.path = []
+
     def rotateRight(self):
         self.image = pygame.transform.rotate(self.image, -90)
-        self.directions= self.directions[1:]+ [self.directions[0]]
+        self.directions = self.directions[1:] + [self.directions[0]]
+
     def rotateLeft(self):
         self.image = pygame.transform.rotate(self.image, 90)
-        self.directions= [self.directions[3]]+self.directions[:3]
+        self.directions = [self.directions[3]] + self.directions[:3]
+
     def move(self):
-        if (self.directions[0]=="N"):
-            self.rect.center = (self.rect.center[0] , self.rect.center[1]- self.size)
-        if (self.directions[0]=="S"):
-            self.rect.center = (self.rect.center[0] , self.rect.center[1] + self.size)
-        if (self.directions[0]=="W"):
-            self.rect.center = (self.rect.center[0] - self.size, self.rect.center[1] )
-        if (self.directions[0]=="E"):
-            self.rect.center = (self.rect.center[0] + self.size, self.rect.center[1] )
-    def goTo(self,endy,endx):
+        if (self.directions[0] == "N"):
+            self.rect.center = (
+                self.rect.center[0], self.rect.center[1] - self.size)
+        if (self.directions[0] == "S"):
+            self.rect.center = (
+                self.rect.center[0], self.rect.center[1] + self.size)
+        if (self.directions[0] == "W"):
+            self.rect.center = (
+                self.rect.center[0] - self.size, self.rect.center[1])
+        if (self.directions[0] == "E"):
+            self.rect.center = (
+                self.rect.center[0] + self.size, self.rect.center[1])
+
+    def goTo(self, endy, endx):
         if(int(map[int(self.rect.y / self.size)][int(self.rect.x / self.size)]) > 0):
             print('asdasdadsada')
-            agentPositionx=self.rect.x / self.size
+            agentPositionx = self.rect.x / self.size
             agentPositiony = self.rect.y / self.size
-            print(agentPositionx,agentPositiony)
-            self.path = astar.search(agentPositionx, agentPositiony, endx, endy, self.directions, map)
+            print(agentPositionx, agentPositiony)
+            self.path = astar.search(
+                agentPositionx, agentPositiony, endx, endy, self.directions, map)
             print(self.path)
-        else: self.path = []
+        else:
+            self.path = []
+
     def update(self):
         if self.path:
-            action=self.path.pop(0)
-            if (action=='rotateRight'): self.rotateRight()
-            if (action == 'rotateLeft'): self.rotateLeft()
-            if (action == 'move'): self.move()
+            action = self.path.pop(0)
+            if (action == 'rotateRight'):
+                self.rotateRight()
+            if (action == 'rotateLeft'):
+                self.rotateLeft()
+            if (action == 'move'):
+                self.move()
+
 
 pygame.init()
-screen = pygame.display.set_mode((1200, 800))
+screen = pygame.display.set_mode((1200, 900))
 
 
-map= loadMap()
+map = loadMap()
 
-grid=Grid(map,40)
+grid = Grid(map, 40)
 
 all_sprites = pygame.sprite.Group()
 agent = Agent(grid.getCellSize())
@@ -107,7 +138,7 @@ all_sprites.add(agent)
 
 clock = pygame.time.Clock()
 while True:
-    screen.fill((0, 0, 0))
+    screen.fill((255, 255, 255))
     grid.draw(screen)
 
     all_sprites.update()
@@ -122,8 +153,8 @@ while True:
             for i in range(grid.ysize):
                 for j in range(grid.xsize):
                     if(grid.cells[i][j].collidepoint(pos)):
-                        print(i,j)
+                        print(i, j)
                         agent.goTo(i, j)
                         break
     pygame.display.update()
-    clock.tick(20)
+    clock.tick(50)
